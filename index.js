cb803ff260ee2c5926cbb840e206c013d2c88b5f
temikis <temikis@mail.ru>
2023-06-29 13:25:50 +0200

    add eslint

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import _ from "lodash";
+import _ from 'lodash';
 
-console.log("Hello, Hexlet!");
-console.log("Correct!");
+console.log('Hello, Hexlet!');
+console.log('Correct!');
 
-console.log(_.last(["one", "two"]));
+console.log(_.last(['one', 'two']));
